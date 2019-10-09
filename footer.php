62f4e41db9acd0f63b4891d99349b58fec164e81
Matthijs1 <214542@edu.rocfriespoort.nl>
2019-10-09 13:34:10 +0200

    header en footer

--- footer.php
+++ footer.php
@@ -1,6 +1,14 @@
 		<!-- Footer section -->
-		<footer>
-			
+		<footer class="footer">
+			<div class="container">
+					<div class="row">
+						<div class="mx-auto text-center">
+							<img src="\wordpress\wp-content\themes\Teezr\assets\img\teezr-footer.png" id="teezr-footer" alt="">
+						</div>
+					</div>
+					<hr id="footer-lijn">
+					<div class="text-center"><a style="color: #000;">Copyright - TEEZR TEAM </a></div>
+			</div>
 		</footer>
 		<!-- End Section -->
 
--- header.php
+++ header.php
@@ -2,32 +2,39 @@
 <html <?php language_attributes(); ?>>
 
 	<head>
-		
+
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+		<script src="\wordpress\wp-content\themes\Teezr\assets\js\app.js">
+
+		</script>
 
 		<?php wp_head(); ?>
 
 	</head>
 
 	<body>
-	
-		<header>
-			<nav class="main-navigation navbar navbar-expand-lg navbar-dark bg-dark">
-				<div class="site-branding-text text-white">
-					(Logo)
-				</div>
-				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#primary-menu-wrap" aria-controls="primary-menu-wrap" aria-expanded="false" aria-label="Toggle navigation">
-					<span class="navbar-toggler-icon"></span>
-				</button>
-				<?php
-					wp_nav_menu( array(
-						'theme_location'  => 'menu-1',
-						'container_class' => 'collapse navbar-collapse',
-						'menu_class'      => 'navbar-nav ml-auto',
-						'depth'           => 2,
-						'walker'          => new WP_bootstrap_4_walker_nav_menu()
-					) );
-				?>
-			</nav>
-		</header>
+
+			<div class="text-center" id="sticky-nav">
+				<img src="/wordpress/wp-content/themes/teezr/assets/img/teezr-logo"  id="teezr-logo">
+					<nav class="navbar navbar-expand-lg navbar-light" style="top:-30px;" >
+						<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+							<div class="navbar-nav">
+							<?php
+								wp_nav_menu( array(
+									'menu_class'      => 'navbar-nav ml-auto',
+									'depth'           => 2,
+									'walker'          => new WP_bootstrap_4_walker_nav_menu()
+								) );
+							?>
+							</div>
+						</div>
+
+						<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+							<span class="navbar-toggler-icon"></span>
+						</button>
+					</nav>
+			</div>
