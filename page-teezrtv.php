a2440252d39cce33f49e9dcee236738fdb5ce899
Matthijs1 <214542@edu.rocfriespoort.nl>
2019-10-18 13:19:05 +0200

    routeplanner update

--- page-teezrtv.php
+++ page-teezrtv.php
@@ -20,15 +20,5 @@ get_header();
 ?>
 
 
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
-    <h1>nfdalfkdsajfkldasfjklsdjfasljflk;dsfjlkasdfjkslajfdlskjfdslkfjdsalkfjdslkds</h1>
-  </body>
-</html>
 
 <?php get_footer(); ?>
